be47ed2c44a28969700ee56f3a626457422dd710
mkohli3 <mkohli3@asu.edu>
2022-10-18 17:09:06 -0700

    added facade classes

---
+++ PTBSFacade.java
@@ -0,0 +1,51 @@
+public class PTBSFacade {
+    import java.util.Iterator;
+    import java.util.Scanner;
+
+    public class HacsFacade {
+        int UserType;
+        Product theSelecteProduct;
+        int nProductCategory;
+        ClassProductList theProductList;
+        Person thePerson;
+
+
+        public boolean login(Login object) {
+            return object.login();
+        }
+        public void addTrading(){
+
+        }
+        public void viewTrading(){
+
+        }
+        public void decideBidding(){
+
+        }
+        public void discussBidding(){
+
+        }
+        public void submitBidding(){
+
+        }
+        public void remind(){
+
+        }
+        public void createUser(){
+
+        }
+        public void createProductList(){
+
+        }
+        public void AttachProductToUser(){
+
+        }
+        public Product selectProduct(){
+
+        }
+        public void productOperation(){
+
+        }
+
+    }
+}
---
+++ ProductMenu.java
@@ -0,0 +1,20 @@
+public class ProductMenu {
+    public void showMenu{
+
+    }
+    public void showAddButton(){
+
+    }
+    public void showViewButton(){
+
+    }
+    public void showRadioButton(){
+
+    }
+    public void showLabels(){
+
+    }
+    public void showComboxes(){
+
+    }
+}
